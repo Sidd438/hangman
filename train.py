6ce486715568fe24d1d037791642bc26dd81832f
Siddharth <f20210438@pilani.bits-pilani.ac.in>
2023-08-14 10:52:15 +0530

    ok

--- train.py
+++ train.py
@@ -8,59 +8,85 @@ import random
 
 def generate_combinations(k, n):
     valid_combinations = []
-    
+
     for comb in combinations(range(1, n + 1), k):
         valid_combinations.append(comb)
-    
+
     return valid_combinations
-
-words = np.loadtxt('words_250000_train.txt', dtype=str)
-lens = np.array([len(w) for w in words])
-percentile_99 = np.percentile(lens, 99)
-# print('99th percentile of word lengths: {}'.format(percentile_99))
-words = words[lens <= percentile_99]
-char_to_int = {char: i for i, char in enumerate('abcdefghijklmnopqrstuvwxyz')}
-vocab_size = len(char_to_int)
-test_words, val_words = train_test_split(words, test_size=0.2, random_state=42)
-words = test_words
-# print('Number of words after filtering: {}'.format(len(words)))
-max_blanks = 9
 alpha_size = 26
-max_word_length = 17
+max_word_length = 15
 model = Sequential()
-model.add(Dense(100, input_shape=(max_word_length,)))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(26, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model = tf.keras.models.load_model("model.h5")
+# model.add(LSTM(128, input_shape=(max_word_length, alpha_size+1), return_sequences=True))
+# # model.add(Dense(128, input_shape=(max_word_length, alpha_size+1), activation='relu'))
+# # model.add(Dense(128, activation='relu'))
+# model.add(LSTM(128, return_sequences=True))
+# model.add(Flatten())
+# model.add(Dense(1920, activation='relu'))
+# # model.add(Dense(128, activation='relu'))
+# model.add(Dense(alpha_size, activation='softmax'))
+# model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+print(model.summary())
+max_blanks = 9
+words_lod = np.loadtxt('words_250000_train.txt', dtype=str)
+char_to_int = {char: i for i, char in enumerate('abcdefghijklmnopqrstuvwxyz_')}
+int_to_char = {i: char for i, char in enumerate('abcdefghijklmnopqrstuvwxyz_')}
 
-output_letters = []
-input_words = []
-for cur, word in enumerate(words[:2500]):
-    combs = []
-    bls = random.sample(range(1, len(word)+1), min(4, len(word)))
-    for i in bls:
-        combs.extend(generate_combinations(i, len(word)))
-    word_seq = [char_to_int[char] for char in word]
-    print(word, word_seq, cur)
-    # print(combs)
-    for comb in combs:
-        temp_seq = list(word_seq)+[26]*(max_word_length-len(word_seq))
-        needs = []
-        for index in comb:
-            needs.append(temp_seq[index - 1])
-            temp_seq[index - 1] = alpha_size
-        for need in needs:
-            input_words.append(temp_seq)
-            output_letters.append(need)
-input_words = np.array(input_words)
-output_letters = np.array(output_letters)
-output_letters = tf.keras.utils.to_categorical(output_letters, num_classes=alpha_size)
-padded = tf.keras.preprocessing.sequence.pad_sequences(input_words, maxlen=max_word_length, padding='post')
-# print(padded)
-# print(output_letters)
-model.fit(padded, output_letters, epochs=3, verbose=0)
+def create_input(seq, max_word_length=15, vocab_sie=27):
+    ans = []
+    for i in seq:
+        buf = [0]*vocab_size
+        buf[char_to_int[i]] = 1
+        ans.append(buf)
+    while(len(ans) < max_word_length):
+        ans.append([0]*vocab_sie)
+    return ans
+
+
+for kl in range(1000):
+    lens = np.array([len(w) for w in words_lod])
+    # print('99th percentile of word lengths: {}'.format(percentile_99))
+    words_lod = words_lod[lens <= max_word_length]
+    vocab_size = len(char_to_int)
+    test_words, val_words = train_test_split(words_lod, test_size=0.1, random_state=random.randint(0, 100000))
+    words = test_words
+    # print('Number of words after filtering: {}'.format(len(words)))
+    output_letters = []
+    input_words = []
+    from collections import defaultdict
+    for cur, word in enumerate(words[:100000]):
+        input_seq = ["_"]*(len(word))
+        seter = set(word)
+        inputer = create_input(input_seq)
+        target = random.choice(list(seter))
+        input_words.append(inputer)
+        output_letters.append(char_to_int[target])
+        seter.remove(target)
+        while(len(seter)):
+            for i in range(len(word)):
+                if word[i] == target:
+                    input_seq[i] = target
+            inputer = create_input(input_seq)
+            target = random.choice(list(seter))
+            input_words.append(inputer)
+            output_letters.append(char_to_int[target])
+            seter.remove(target)
+    print(len(input_words))
+    input_words = np.array(input_words)
+    output_letters = np.array(output_letters)
+    output_letters = tf.keras.utils.to_categorical(output_letters, num_classes=alpha_size)
+    print(output_letters.shape)
+
+    # print(len(input_words))
+    # input_words = np.array(input_words)
+    # output_letters = np.array(output_letters)
+    # output_letters = tf.keras.utils.to_categorical(output_letters, num_classes=alpha_size)
+    # print(output_letters.shape)
+    # padded = tf.keras.preprocessing.sequence.pad_sequences(input_words, maxlen=max_word_length, padding='post')
+    # print(padded)
+    # print(output_letters)
+    model.fit(input_words, output_letters, epochs=1, verbose=1)
     # break
-print(output_letters)
+    model.save('model.h5')
 print('Training complete')
-
-model.save('model.h5')
+# print(test_words[0])
